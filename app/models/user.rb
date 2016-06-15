6a7bd60e6d4f13afa1d6e254f671841137f0c367
Ben Stein <bumstain@gmail.com>
2016-06-15 11:01:02 +0100

    Adjusted Devise to accept Instagram nickname for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:instagram]
 
+  def email_required? 
+    false
+  end
+
   def self.from_omniauth(auth)
-    response = auth.env["omniauth.auth"].credentials
-    # User.create(email: auth.code, password: "123456", password_confirmation: "123456" )
+    auth = auth.env["omniauth.auth"]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.uid = auth.uid
+      # user.email = FFaker::Internet.email 
+      user.provider = auth.provider 
+      user.username = auth.info.nickname 
+      user.password = Devise.friendly_token[0,20]
+    end
+      # User.create(username: response.info.nickname, password: (Devise.friendly_token[0,20]))
   end
 end
