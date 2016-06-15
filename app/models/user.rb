bf1faf668d47a50a45501af01fca74ee6dcb271d
Ben Stein <bumstain@gmail.com>
2016-06-15 11:25:03 +0100

    Cleaned up code and directories to pass Travis build

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,9 @@ class User < ApplicationRecord
     auth = auth.env["omniauth.auth"]
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.uid = auth.uid
-      # user.email = FFaker::Internet.email 
       user.provider = auth.provider 
       user.username = auth.info.nickname 
       user.password = Devise.friendly_token[0,20]
     end
-      # User.create(username: response.info.nickname, password: (Devise.friendly_token[0,20]))
   end
 end
