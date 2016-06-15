f44a6dbd29515d1174c910ea160ba89c77808263
Max Provin <maxprovin@outlook.com>
2016-06-15 14:51:40 +0100

    search controller, create action updated

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -14,6 +14,21 @@ class Api::V1::SearchesController < ApplicationController
     end
   end
 
+#   def create
+#   @user = User.new(params[:user])
+
+#   respond_to do |format|
+#     if @user.save
+#       flash[:notice] = 'User was successfully created.'
+#       format.html { redirect_to(@user) }
+#       format.xml { render xml: @user, status: :created, location: @user }
+#     else
+#       format.html { render action: "new" }
+#       format.xml { render xml: @user.errors, status: :unprocessable_entity }
+#     end
+#   end
+# end
+
   private
 
   def search_params
